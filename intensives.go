924e71425d136ad375d4a4d85d3dbbf53e5111e6
Elizaveta <lizabettus@gmail.com>
2023-08-14 18:51:57 +0300

    pick sort

--- intensives.go
+++ intensives.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := []int{1, 5, 7, 3, 4, 6, 8, 0}
+	PickSort(arr)
+}
+
+func BubbleSort(arr []int) {
+	for i := 0; i+1 < len(arr); i++ {
+		for j := 0; j+1 < len(arr)-i; j++ {
+			if arr[j] > arr[j+1] {
+				k := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = k
+			}
+		}
+	}
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Print(arr[i])
+	}
+}
+
+func PickSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		min := arr[i]
+		min_i := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < min {
+				min = arr[j]
+				min_i = j
+			}
+		}
+		if min_i != i {
+			k := arr[i]
+			arr[i] = min
+			arr[min_i] = k
+		}
+	}
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Print(arr[i])
+	}
 }
